6c8f0de0eec55d1e0ca55edb4e259431ba235a6d
shihab khan <shihabx7@gmail.com>
2022-11-08 17:53:46 +0600

    up

---
+++ comps/activitiyPageComps/GameFaq.js
@@ -0,0 +1,34 @@
+import FaqList from "../faqComps/FaqList"
+import TitleSeparator from "../util/TitleSeparator"
+const GameFaq=(props)=>{
+
+
+    return(
+        <div id="game-faq" className="all-faq relative bg-black py-16 md:py-24 lg:py-32 px-4 overflow-hidden md:mt-[0px]" style={{
+
+            background:'url("/assets/svg/gift-bg.svg"), linear-gradient(65deg, rgb(0, 0, 0), rgb(0, 0, 0))',
+            backgroundPosition:"bottom center,center center",
+            backgroundRepeat:"no-repeat"
+        }}>
+            <div className="gift-lt max-w-[72px] md:max-w-[120px] lg:max-w-[130px] opacity-[0.4] absolute top-0 left-0"><img className="w-full" src="/assets/faq-q.png"></img></div>
+             <div className="gift-rt absolute"><img src="/assets/Compas-brown.png"></img></div>
+                 <div className="max-w-7xl mx-auto relative z-30">
+                     <div className="section-title mb-8 md:mb-12">
+                          <TitleSeparator title="HAVE QUESTIONS ABOUT HOW THE GAME SHOW WORKS? " color="golden-text" weight="font-bold"/>
+                          <p className="mt-4 text-gray-100 max-w-[800px] mx-auto text-center lg:text-lg">No worries. Your professional game host will explain the rules before each game, emcee all game questions, and enthusiastically guide playing contestants through a series of fun challenges until a winner is revealed at the end of the show. Here are the FAQs.</p>
+
+                      </div>
+                    <div className="gift-faq-box max-w-[1000px] mx-auto"> 
+
+                        <FaqList faqlist={props.faqlist}/>
+
+                    </div>
+                
+                     
+                </div>
+        </div>
+    )
+
+}
+
+export default GameFaq
--- pages/albany-ny/activities/[activitySlug].js
+++ pages/albany-ny/activities/[activitySlug].js
@@ -1,5 +1,4 @@
 
-
 import Footer from '../../../comps/Footer';
 import Homenav from '../../../comps/Homenav';
 
@@ -12,8 +11,8 @@ import BreadcumNew from '../../../comps/util/BreadcumNew';
 import GameLocDetails from '../../../comps/activitiyPageComps/GameLocDetails';
 import GameGallery from '../../../comps/activitiyPageComps/GameGallery';
 import GameVideo from '../../../comps/activitiyPageComps/GameVideo';
-import FaqList from '../../../comps/faqComps/FaqList';
-import TitleSeparator from '../../../comps/util/TitleSeparator';
+import GameFaq from '../../../comps/activitiyPageComps/GameFaq';
+
 
 const LocSingleActivity=(props)=>{
    
@@ -51,31 +50,11 @@ const LocSingleActivity=(props)=>{
                         <GameGallery galleryitem={props.activitygallery} activityname={props.pagedata.activityname} category={props.activitydata.category}/>
                         <GameVideo videoid={props.activitydata.videoid} location={props.pagedata.locationslug}/>
                         {
-            props.activitydata.faqlist &&
-            <div id="game-faq" className="all-faq relative bg-black py-16 md:py-24 lg:py-32 px-4 overflow-hidden md:mt-[0px]" style={{
-
-                background:'url("/assets/svg/gift-bg.svg"), linear-gradient(65deg, rgb(0, 0, 0), rgb(0, 0, 0))',
-                backgroundPosition:"bottom center,center center",
-                backgroundRepeat:"no-repeat"
-            }}>
-                <div className="gift-lt max-w-[72px] md:max-w-[120px] lg:max-w-[130px] opacity-[0.4] absolute top-0 left-0"><img className="w-full" src="/assets/faq-q.png"></img></div>
-                 <div className="gift-rt absolute"><img src="/assets/Compas-brown.png"></img></div>
-                     <div className="max-w-7xl mx-auto relative z-30">
-                         <div className="section-title mb-8 md:mb-12">
-                              <TitleSeparator title="HAVE QUESTIONS ABOUT HOW THE GAME SHOW WORKS? " color="golden-text" weight="font-bold"/>
-                              <p className="mt-4 text-gray-100 max-w-[800px] mx-auto text-center lg:text-lg">Browse through these FAQs to find answers to commonly raised questions. If you don't see what's on your mind, reach out to us anytime on phone or email.</p>
-    
-                          </div>
-                        <div className="gift-faq-box max-w-[1000px] mx-auto"> 
-    
-                            <FaqList faqlist={props.activitydata.faqlist}/>
-    
-                        </div>
-                    
-                         
-                    </div>
-            </div>
-        }
+                          props.activitydata.faqlist &&
+                                <>
+                                    <GameFaq faqlist={props.activitydata.faqlist}/>
+                                </>
+                         }
                        
  
                  </div>
