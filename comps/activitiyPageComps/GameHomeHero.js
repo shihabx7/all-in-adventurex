1498fbfe5d12c17eac1e9b2a5e17e391e62ee4e4
shihab khan <shihabx7@gmail.com>
2022-12-27 03:20:43 +0600

    update hero

--- comps/activitiyPageComps/GameHomeHero.js
+++ comps/activitiyPageComps/GameHomeHero.js
@@ -9,8 +9,8 @@ const GameHomeHero=(props)=>{
             }
            }>
         <div className='home-hero-holder '> 
-        <div className="homehero-img h-[66%] bg-[#111111] md:hidden">
-                  <img src={props.pagedata.coverimageM}></img>
+        <div className="homehero-img w-full min-h-[220px] bg-[#111111] md:hidden">
+                  <img className="w-full" src={props.pagedata.coverimageM}></img>
              </div>
             <div className='max-w-7xl home-hero-container mx-auto px-4 pt-0 pb-20 md:py-20 lg:py-32 xl:py-40 relative z-20'>
                 <div className="page-benar-info"> 
