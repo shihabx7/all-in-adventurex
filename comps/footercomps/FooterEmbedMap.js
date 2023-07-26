7b3c24c7586fa911f362dd320f5b8b99838a607a
shihab khan <shihabx7@gmail.com>
2023-07-26 20:28:53 +0600

    update embed map

--- comps/footercomps/FooterEmbedMap.js
+++ comps/footercomps/FooterEmbedMap.js
@@ -1,15 +1,18 @@
-const FooterEmbedMap=()=>{
-
-    return (
-        <div className=" overflow-hidden pb-8 md:pb-10 lg:pb-12">
-             <div className="space w-full h-[4px] bg-[#111111] relative z-10"> </div>
-                 <div className="mt-[-60px]"> 
-                 <div className="w-full h-[520px] md:h-[640px] lg:h-[720px]"> 
-                 <iframe src="https://www.google.com/maps/d/embed?mid=1XniDBpW7F60KkSK_e7i7VhyDf8AzfjY&ehbc=2E312F" width="100%" height="100% "></iframe>
-                </div>
-                </div>
+const FooterEmbedMap = () => {
+  return (
+    <div className=" overflow-hidden pb-8 md:pb-10 lg:pb-12">
+      <div className="space w-full h-[4px] bg-[#111111] relative z-10"> </div>
+      <div className="mt-[-60px]">
+        <div className="w-full h-[520px] md:h-[640px] lg:h-[720px]">
+          <iframe
+            src="https://www.google.com/maps/d/embed?mid=1XniDBpW7F60KkSK_e7i7VhyDf8AzfjY&ehbc=2E312F"
+            width="100%"
+            height="100% "
+          ></iframe>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default FooterEmbedMap
+export default FooterEmbedMap;
