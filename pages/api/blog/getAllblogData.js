09c311427e2023e3b81d5e1e19441a9e8c50c5ee
shihab khan <shihabx7@gmail.com>
2023-08-27 17:28:18 +0600

    update new blog

--- pages/api/blog/getAllblogData.js
+++ pages/api/blog/getAllblogData.js
@@ -4,6 +4,144 @@ export const AllblogData = () => {
 };
 
 const AllblogDataobj = {
+  "skills-that-escape-room-games-can-improve": {
+    pagemeta: {
+      title: "SKILLS THAT ESCAPE ROOM GAMES CAN IMPROVE",
+      description:
+        "Escape rooms have steadily become one of the most vibrant indoor attractions in the United States and around the globe. Not only are escape rooms entertaining, but they also inspire participants to discover their dormant skills.",
+      keywords:
+        "escape room skills,escape room tips and tricks,escape room guide, play an escape room,book escape room, escape room news, all in adventures articles,",
+      url: "skills-that-escape-room-games-can-improve",
+      metaindex: true,
+      metaimg:
+        "/assets/blogs/hero-skills-that-escape-room-games-can-improve.jpg",
+    },
+    pagedata: {
+      pagetitle: "SKILLS THAT ESCAPE ROOM GAMES CAN IMPROVE",
+      description:
+        "Escape rooms have steadily become one of the most vibrant indoor attractions in the United States and around the globe. Not only are escape rooms entertaining, but they also inspire participants to discover their dormant skills.",
+      ftimg: "/assets/blogs/hero-skills-that-escape-room-games-can-improve.jpg",
+    },
+    bloginfo: {
+      authimg: "/assets/blogs/blog-auth1.png",
+      authname: "Brian Capps",
+      popular: false,
+      blogcategory: [
+        {
+          id: "1",
+          name: "Escape Room",
+          slug: "escape-room",
+        },
+        {
+          id: "2",
+          name: "Tips & tricks",
+          slug: "tips-and-tricks",
+        },
+      ],
+      createdate: "July 08, 2023",
+      lastupdate: "July 08, 2023",
+    },
+    blogdesc: [
+      '<div class="sbl-block">',
+      "<p>Escape rooms have steadily become one of the most vibrant indoor attractions in the United States and around the globe. Not only are escape rooms entertaining, but they also inspire participants to discover their dormant skills. </p>",
+      "<p>Learn <a href='/what-is-an-escape-room'>what an escape room is</a> if you are unfamiliar with the concept.</p>",
+      "<p>Escape games not only improve team-building, leadership, and communication but also enhances many brain functions as well. Remember that you can advance some valuable skills even more if you know what they are beforehand.</p>",
+      "<p>Following are some of the most notable skills that playing escape room games can drastically improve</p>",
+      "</div>",
+
+      "<div class='sbl-block'>",
+      "<h3>ENHANCED TIME MANAGEMENT SKILLS</h3>",
+      "<p>Playing an escape room will help you enhance your time management skills, unlike any other games out there. When your time is limited, you are already under pressure to find clues and solve puzzles; every minute counts.</p>",
+      "<p>Every puzzle, riddle and clue is designed to be solved and found within the given time. So, you will quickly learn how to manage your time efficiently to beat the game and the clock. There is no doubt that this is an absolutely necessary skill in the times we live in today, where everything has to be on time down to the second.</p>",
+
+      "<h3>CREATIVE AND CRITICAL THINKING</h3>",
+      "<p><img alt='creative and critical thinking' src='/assets/blogs/creative-and-critical-thinking.jpg'/></p>",
+      "<p>You are compelled to be creative when you only have 50-60 minutes on the clock. Everything you see in the room, from the mirror on the wall to the secret door, all contain possible solutions to figuring out the complex puzzle.</p>",
+      "<p>Your critical thinking skills are used and enhanced when finding creative solutions to solve a problem. The beauty of an escape room is that you must analyze patterns and connect the dots in the game to solve a problem. Creative thinking allows you to think outside the box and find the solution. </p>",
+      "<p>Enrich your critical thinking today! Find your nearest <a href='/locations'>All In Adventures location</a>. We have 26 locations all over the United States.</p>",
+
+      "<h3>PROBLEM-SOLVING CAPACITY</h3>",
+
+      "<p>Confronted by seemingly unsolvable scenarios are part of an escape room's charm. With each activity you are presented with, you must use your problem-solving abilities that will also apply to real-life situations. </p>",
+      "<p>Without a basis in reality, escape rooms don't work. So, be prepared to step into another world rooted in what you know but with a compelling mystery to unlock your problem-solving brain!</p>",
+
+      "<h3>ENHANCED COMMUNICATION SKILLS</h3>",
+      "<p>Communication itself is an art. From the shy introvert to the confident extrovert, the design of an escape room is such that you need to be vocal, communicate with your teammates, and participate in the team effort. On the other hand, listening is also a big part of communication, so be sure to listen to your teammates to ensure effective communication.</p>",
+      "<p>Remember that every observation counts; it is possible that you have noticed something that other teammates have not. Playing an escape game will teach you how effective communication can be when it comes to tackling a task as a group.</p>",
+
+      "<h3>BE A BETTER TEAM PLAYER</h3>",
+      "<p><img alt='be a better team player' src='/assets/blogs/be-a-better-team-player.jpg'/></p>",
+      "<p>Solving an escape room is not meant to be done alone. By design, puzzles are often set up with two or more players in mind so that tasks are to be solved collectively. What's more, you are united with a common goal: to escape the room within the allocated 50-60 minutes. </p>",
+      "<p>Playing to each other's strengths and working together will give you the most excellent chance of coming out with a win. It is a precious life skill to gain when reuniting with the outside world!</p>",
+
+      "<h3>IMPROVED CONCENTRATION</h3>",
+      "<p>Learning to focus during stressful time-restricted moments is one of the biggest challenges you can face. Within an escape room, being able to concentrate is paramount. </p>",
+      "<p>Most escape games you encounter will demand your concentration throughout the entire game if you really want to escape the room, and this will help you expand your attention span and keep your concentration focused on the task at hand.</p>",
+
+      "<h3>ATTENTION TO DETAIL</h3>",
+      "<p><img alt='attention to detail' src='/assets/blogs/attention-to-detail.jpg'/></p>",
+      "<p>Remember that everything in an escape room has a purpose. If you have played an escape room game before, you might have noticed that some crucial clues are always in the detail. Awareness is the key in any game, and an escape room is no exception. You must constantly be aware of what is happening around; you never know when you might stumble upon the game-winning element.</p>",
+      "<p>So, your exercise of paying attention to detail will help you a great deal in the long run. It's an essential skill that will come in handy in almost any fi</p>",
+      "<p>Give the gift to develop real-life skills; All In Adventure <a href='/gift-cards'>gift cards</a> never expire.</p>",
+
+      "</div>",
+
+      '<div class="sbl-block">',
+      "<h2>HOW TO BOOK YOUR NEXT EVENT</h2>",
+      "<p>Looking for an escape room adventure? Look no further than <a href='/locations'>All In Adventures</a> for your next escape room experience! Book now and prepare to be fully immersed in a thrilling world of puzzles, challenges, and excitement.</p>",
+      "<p>Click the &quot;BOOK GAMES&quot; button and choose your location to get an instant online booking for a group of 2-10 people.</p>",
+      "<p><a href='/events'>Planning an event</a> with a large group of over 10 people? We've got you covered! Contact one of our <a href='/locations'>local stores</a>, and we will take you through the process and get you our special deals.</p>",
+      "</div>",
+    ],
+    relatedblogdata: [
+      {
+        id: "1",
+        slug: "what-kind-of-leaf-is-this",
+        ftimg: "/assets/blogs/blog-ft.jpg",
+        title: "What kind of leaf is this?",
+        blogdesc:
+          "You'd better baylieve that the game is going to get harder from here! You'd better baylieve that the game is going to get harder from here! You'd better",
+        authimg: "/assets/blogs/blog-auth1.png",
+        authname: "by Brian Capps",
+        blogcategory: [
+          {
+            id: "1",
+            name: "Escape Room",
+            slug: "escape-room",
+          },
+          {
+            id: "2",
+            name: "Team Building",
+            slug: "team-building",
+          },
+        ],
+        lastupdate: "April 02, 2022",
+      },
+      {
+        id: "2",
+        slug: "what-kind-of-leaf-is-this",
+        ftimg: "/assets/blogs/blog-ft.jpg",
+        title: "1. What kind of leaf is this What kind of leaf is this?",
+        blogdesc:
+          "You'd better baylieve that the game is going to get harder from here! You'd better baylieve that the game is going to get harder from here! You'd better",
+        authimg: "/assets/blogs/blog-auth1.png",
+        authname: "by Brian Capps",
+        blogcategory: [
+          {
+            id: "1",
+            name: "Escape Room",
+            slug: "escape-room",
+          },
+          {
+            id: "2",
+            name: "Team Building",
+            slug: "team-building",
+          },
+        ],
+        lastupdate: "May 18, 2022",
+      },
+    ],
+  },
   "innovative-team-building-ideas-to-strengthen-bonds": {
     pagemeta: {
       title: "INNOVATIVE TEAM-BUILDING IDEAS TO STRENGTHEN BONDS",
