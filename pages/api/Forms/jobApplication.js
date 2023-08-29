490b41d9c2566f301207e2abe5cea873f756df7b
shihab khan <shihabx7@gmail.com>
2023-08-29 16:53:06 +0600

    up form

--- pages/api/Forms/jobApplication.js
+++ pages/api/Forms/jobApplication.js
@@ -104,8 +104,8 @@ export default async function jobApplicationHandler(req, res) {
   try {
     await transporter.sendMail({
       from: "sender@allinadventures.com",
-      to: "shihab.dgency@gmail.com",
-      //to: "careers@allinadventures.com",
+      //to: "shihab.dgency@gmail.com",
+      to: "careers@allinadventures.com",
       bcc: "dgency.com@gmail.com",
       //  bcc: "dgency.com@gmail.com",
       subject: `Job Application - ${retbody.info1.lName} ${retbody.info1.fName}`,
