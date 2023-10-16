79474595c2a610afe2f4c4ed15c5e47aa763a5e4
shihab khan <shihabx7@gmail.com>
2023-10-16 18:39:14 +0600

    update with pdf creator

--- pages/api/Forms/creatPdfApplication.js
+++ pages/api/Forms/creatPdfApplication.js
@@ -5,90 +5,62 @@ import handlebars from "handlebars";
 //import pdf from html-pdf
 
 export async function creatPdfApplication(pdfData) {
-  var templateHtml = fs.readFileSync(
-    path.join(process.cwd(), "pdf/pdfhtml.html"),
-    "utf8"
-  );
-  var options = {
-    format: "Letter",
-    orientation: "portrait",
-    border: "12mm",
-    header: {
-      height: "4mm",
-      //contents: '<div style="background:#FFF7EA;"><p style="margin:0px;">Job application form</p> <p style="margin:4px; color:#888888">'+pdt+'</p></div>'
-    },
-    footer: {
-      height: "2mm",
-      //  contents: {
-      // //     first: '',
-      //      2: '', // Any page number is working. 1-based index
-      //      default: '', // fallback value
-      //      last: ''
-      //   }
-    },
-  };
-  var logo = fs.readFileSync(path.join(process.cwd(), "pdf/tik.jpg"), "base64");
+  try {
+    const html = fs.readFileSync(
+      path.join(process.cwd(), "pdf/pdfhtml.html"),
+      "utf8"
+    );
+    var dt = new Date();
+    var mn = dt.getMonth() + 1;
+    var pdt = mn + "_" + dt.getDate() + "_" + dt.getFullYear();
+    var pubdate = mn + "-" + dt.getDate() + "-" + dt.getFullYear();
+    var d = new Date();
+    var fileTxtname =
+      pdfData.info1.fName.trim().split(" ").join("_") +
+      "_" +
+      pdfData.info1.lName.trim().split(" ").join("_");
 
-  var dt = new Date();
-  var mn = dt.getMonth() + 1;
-  var pdt = mn + "_" + dt.getDate() + "_" + dt.getFullYear();
-  var pubdate = mn + "-" + dt.getDate() + "-" + dt.getFullYear();
-  var d = new Date();
-  var fileTxtname =
-    pdfData.info1.fName.trim().split(" ").join("_") +
-    "_" +
-    pdfData.info1.lName.trim().split(" ").join("_");
+    var pdfnpath =
+      "pdf/" +
+      fileTxtname +
+      "_" +
+      pdt +
+      "_" +
+      d.getFullYear() +
+      d.getDate() +
+      d.getHours() +
+      d.getMinutes() +
+      d.getMilliseconds() +
+      ".pdf";
+    var logo = fs.readFileSync(
+      path.join(process.cwd(), "pdf/tik.jpg"),
+      "base64"
+    );
+    var outpath = path.join(process.cwd(), pdfnpath);
+    const options = {
+      format: "A4",
+      orientation: "portrait",
+      border: "8mm",
+      header: {
+        height: "1mm",
+        // contents: '<div style="text-align: center;">Header</div>',
+      },
+      footer: {
+        height: "1mm",
+      },
+    };
 
-  var pdfnpath =
-    "pdf/" +
-    fileTxtname +
-    "_" +
-    pdt +
-    "_" +
-    d.getFullYear() +
-    d.getDate() +
-    d.getHours() +
-    d.getMinutes() +
-    d.getMilliseconds() +
-    ".pdf";
-
-  var outpath = path.join(process.cwd(), pdfnpath);
-  var document = {
-    html: templateHtml,
-    data: {
-      info: pdfData,
-      tik: logo,
-      pubdate: pubdate,
-    },
-    path: outpath,
-    type: "",
-  };
-  let retData = {
-    pdfpath: "",
-    success: false,
-  };
-
-  const pdfc = pdf.create(document, options);
-  /*
-    .then((res) => {
-      //console.log(res);
-      retData.pdfpath = outpath;
-      retData.success = true;
-      retData.msg = res;
-    })
-    .catch((error) => {
-      retData.pdfpath = outpath;
-      retData.success = false;
-      retData.msg = error;
-      //console.error(error);
-    }); */
-  if (!pdfc) {
-    retData.pdfpath = outpath;
-    retData.success = false;
-  } else {
-    retData.pdfpath = outpath;
-    retData.success = true;
+    const document = {
+      html: html,
+      data: { info: pdfData, tik: logo, pubdate: pubdate },
+      path: outpath,
+      type: "pdf",
+    };
+    await pdf.create(document, options);
+    console.log("PDF generated successfully. PDF path:", outpath);
+    return { success: true, pdfPath: outpath };
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+    return { success: false, error: error.message };
   }
-
-  return retData;
 }
