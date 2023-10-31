6fa589795f9c49593ff3aa2574c062ae536e246c
shihab khan <shihabx7@gmail.com>
2023-10-31 21:48:34 +0600

    update hero

--- comps/activitiyPageComps/GameLocHero.js
+++ comps/activitiyPageComps/GameLocHero.js
@@ -104,7 +104,7 @@ const GameLocHero = (props) => {
               }
             ></img>
           </div>
-          <div className="max-w-7xl home-hero-container mx-auto px-4 pt-0 pb-[130px] md:pt-20 md:pb-28 lg:py-32 xl:py-36 relative z-50">
+          <div className="max-w-7xl home-hero-container mx-auto px-4 pt-0 pb-[130px] md:pt-20 md:pb-28 lg:py-32 xl:py-36 relative z-30">
             <div className="page-benar-info">
               {props.pagedata.publish_status == false && (
                 <div className="text-center font-bold text-red-600 text-[32px] md:text-[52px] font-os cm-title py-4">
