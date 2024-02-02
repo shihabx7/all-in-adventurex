61506bcb1aad4a6a4181a21575126a026b599fbc
shihab khan <shihabx7@gmail.com>
2024-02-02 13:23:12 +0600

    update escape from alc data

--- pages/api/LocationIndData/getLocationIndActivityList.js
+++ pages/api/LocationIndData/getLocationIndActivityList.js
@@ -739,224 +739,6 @@ const LocActivityData = {
     totalFiveStarReview: "6.9k",
     totalPlayerEscaped: "12k+",
 
-    activity_list2: [
-      {
-        id: 1,
-        activity_name: "Escape from Alcatraz",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "8",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "You are prison guards at the maximum security prison, Alcatraz and the Warden's keys have gone missing on your watch! Can you find them before this turns into an escape attempt?",
-        activity_slug: "escape-from-alcatraz",
-        activity_cover_image:
-          "/assets/all-game-list/escape-from-alcatraz-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 2,
-        activity_name: "Treasure Island",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "10",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "You've been captured by a notorious gang of pirates. Prove you're worthy of becoming part of the crew or prepare to walk the plank!",
-        activity_slug: "treasure-island",
-        activity_cover_image:
-          "/assets/all-game-list/treasure-island-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 3,
-        activity_name: "Zombie Apocalypse",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "12",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "You are trapped in a lab lockdown with a growing zombie horde and need to find your way out before it is too late!",
-        activity_slug: "zombie-apocalypse",
-        activity_cover_image:
-          "/assets/all-game-list/zombie-apocalypse-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 4,
-        activity_name: "Sherlock's Library",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "10",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "London's top detective duo, Sherlock Holmes & Dr. John H. Watson, have been called upon to solve their next case. As detectives work with Watson to solve the mystery and name the guilty suspect responsible for these crimes committed.",
-        activity_slug: "sherlocks-library",
-        activity_cover_image:
-          "/assets/all-game-list/sherlocks-library-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 5,
-        activity_name: "Black Ops",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "8",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "After acquiring all the necessary evidence, it is time to execute your mission. Infiltrate the enemy bunker and apprehend the arms dealer known as The Hawk.",
-        activity_slug: "black-ops",
-        activity_cover_image:
-          "/assets/all-game-list/black-ops-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 6,
-        activity_name: "Superhero's Adventure",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "8",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "The arch-criminal Matador and his team of henchmen have devised an evil plan to take over the city. You must use your superhero powers to help save the day!",
-        activity_slug: "superheros-adventure",
-        activity_cover_image:
-          "/assets/all-game-list/superheros-adventure-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 7,
-        activity_name: "HOUDINI'S MAGIC CELL",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "10",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "As an aspiring magician, you have always looked up to Houdini and his impressive skills. Now, it is your chance to prove you have what it takes to be a Master Magician!",
-        activity_slug: "houdinis-magic-cell",
-        activity_cover_image:
-          "/assets/all-game-list/houdinis-magic-cell-all-in-adventures-escape-room.jpg",
-      },
-
-      {
-        id: 8,
-        activity_name: "Special Agent",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "8",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "An international jewel thief is about to slip free, can you and your team stop them from escaping before it is too late?",
-        activity_slug: "special-agent",
-        activity_cover_image:
-          "/assets/all-game-list/special-agent-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 9,
-        activity_name: "HOLLYWOOD PREMIERE",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "8",
-        price: "29.89",
-        duration: "50",
-        activity_desc:
-          "Live from Hollywood, California, you're the next lucky contestant on our exciting game show! Solve our puzzles and see if you can name our secret celebrity!",
-        activity_slug: "hollywood-premiere",
-        activity_cover_image:
-          "/assets/all-game-list/hollywood-premiere-all-in-adventures-escape-room.jpg",
-      },
-      {
-        id: 12,
-        activity_name: "Locked in the Lift",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "4",
-        price: "29.89",
-        duration: "30",
-        activity_desc:
-          "Welcome to the 40's! You are trapped in a luxurious lift meant only for the most exclusive VIPs. See if you can piece together how to get things running again.",
-        activity_slug: "locked-in-the-lift",
-        activity_cover_image:
-          "/assets/all-game-list/locked-in-the-lift-all-in-adventures-escape-room.jpg",
-      },
-      /*
-      {
-        id: 13,
-        activity_name: "MAGIC CASTLE",
-        activity_type: "In-person",
-        activity_category: "escapegame",
-        age: "6-99",
-        min_player: "2",
-        max_player: "4",
-        price: "29.89",
-        duration: "60",
-        activity_desc:
-          "Welcome to the Magic Castle! Pass your entrance exam to prove you belong among the most prestigious wizards. Test your wits, courage, loyalty, and cunning.",
-        activity_slug: "magic-castle",
-        activity_cover_image:
-          "/assets/all-game-list/magic-castle-all-in-adventures-escape-room.jpg",
-      },
-*/
-      {
-        id: 14,
-        activity_name: "Beat The Seat",
-        activity_type: "In-person",
-        activity_category: "other",
-        age: "6-99",
-        min_player: "1",
-        max_player: "1",
-        price: "5",
-        duration: "2",
-        activity_desc:
-          "It's a 2 minute fun filled, exhilarating experience where you will test your endurance, coordination and ability to think on your feet. ",
-        activity_slug: "beat-the-seat",
-        activity_cover_image:
-          "/assets/all-game-list/allinadventures-beat-the-seat.jpg",
-      },
-      {
-        id: 17,
-        activity_name: "Virtual game show room",
-        activity_type: "Virtual",
-        activity_category: "other",
-        age: "6-99",
-        min_player: 4,
-        max_player: 12,
-        price: "20",
-        duration: "60",
-        activity_desc:
-          "An interactive live game show experience that players of all ages and backgrounds can come together and play from across the globe",
-        activity_slug: "virtual-games/virtual-game-show-room",
-        activity_cover_image:
-          "/assets/all-game-list/All-In-Adventures-Virtual-Game-Show-Room.jpg",
-        booking: {
-          shortname: "virtualgameshowroom",
-          item: "285707",
-          flow: "594928",
-        },
-      },
-    ],
     activity_list: [
       {
         id: 1,
@@ -966,11 +748,11 @@ const LocActivityData = {
         age: "6-99",
         min_player: "2",
         max_player: "10",
-        success_rate: "56",
+        success_rate: "32",
         price: "29.89",
         duration: "50",
         activity_desc:
-          "You are prison guards at Alcatraz and the Warden's keys have gone missing on your watch! Can you find them before this turns into an escape attempt?",
+          "As a library trustee, you have access to a crew that can help with your escape plan. Will you make it out of your cell, and off this miserable rock?",
         activity_slug: "escape-from-alcatraz",
         activity_cover_image:
           "/assets/all-game-list/escape-from-alcatraz-all-in-adventures-escape-room.jpg",
@@ -2742,11 +2524,11 @@ const LocActivityData = {
         age: "6-99",
         min_player: "2",
         max_player: "10",
-        success_rate: "56",
+        success_rate: "32",
         price: "29.89",
         duration: "50",
         activity_desc:
-          "You are prison guards at Alcatraz and the Warden's keys have gone missing on your watch! Can you find them before this turns into an escape attempt?",
+          "As a library trustee, you have access to a crew that can help with your escape plan. Will you make it out of your cell, and off this miserable rock?",
         activity_slug: "escape-from-alcatraz",
         activity_cover_image:
           "/assets/all-game-list/escape-from-alcatraz-all-in-adventures-escape-room.jpg",
@@ -3483,11 +3265,11 @@ const LocActivityData = {
         age: "6-99",
         min_player: "2",
         max_player: "8",
-        success_rate: "56",
+        success_rate: "32",
         price: "29.89",
         duration: "50",
         activity_desc:
-          "You are prison guards at Alcatraz and the Warden's keys have gone missing on your watch! Can you find them before this turns into an escape attempt?",
+          "As a library trustee, you have access to a crew that can help with your escape plan. Will you make it out of your cell, and off this miserable rock?",
         activity_slug: "escape-from-alcatraz",
         activity_cover_image:
           "/assets/all-game-list/escape-from-alcatraz-all-in-adventures-escape-room.jpg",
