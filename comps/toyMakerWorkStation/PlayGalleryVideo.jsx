3109896f31714ff00ac8e4946e5e30687df5d8ca
shihab khan <shihabx7@gmail.com>
2025-06-15 22:56:55 +0600

    up

--- comps/toyMakerWorkStation/PlayGalleryVideo.jsx
+++ comps/toyMakerWorkStation/PlayGalleryVideo.jsx
@@ -5,15 +5,16 @@ export default function PlayGalleryVideo({ videoUrl, videoPoster }) {
     videoRef.current.play();
   }, []);
   return (
-    <div className="w-full ">
+    <div className="w-fullk rounded-lg">
       <video
         ref={videoRef}
         id={"hero-bg-video"}
         className={
-          "no-fullscreen-vid w-full h-full aspect-video object-cover object-center rounded-lg transition-all duration-500 ease-in-out"
+          " w-full  rounded-lg transition-all duration-500 ease-in-out"
         }
         preload="metadata"
         playsInline
+        controls
         autoPlay
         loop
         muted
