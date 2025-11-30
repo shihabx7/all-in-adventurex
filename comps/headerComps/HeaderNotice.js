f479accdd3f4f245bd2996a4b7dab183784054ef
shihab khan <shihabx7@gmail.com>
2025-12-01 03:32:06 +0600

    up

--- comps/headerComps/HeaderNotice.js
+++ comps/headerComps/HeaderNotice.js
@@ -1,13 +1,41 @@
+import { useState, useEffect } from "react";
 import UnlimitedBundleNotice from "../noticeBaner/UnlimitedBundleNotice";
 import BlackFridayNotice from "../noticeBaner/BlackFridayNotice";
-export default function HeaderNotice({ noticeData, locationSlug,giftBooking }) {
+export default function HeaderNotice({
+  noticeData,
+  locationSlug,
+  giftBooking,
+}) {
+  const [currentNotice, setCurrentNotice] = useState("uer");
+
+  useEffect(() => {
+    const expireDate = new Date("2025-12-01T22:59:59");
+    const currentTime = new Date();
+    const timeRemainingMs = expireDate.getTime() - currentTime.getTime();
+    if (timeRemainingMs > 0) {
+      setCurrentNotice("gift");
+    } else {
+      setCurrentNotice("uer");
+    }
+  }, []);
+
   return (
     <div className="header-notice-container">
-      <BlackFridayNotice
+    {currentNotice=='gift' && <>
+       <BlackFridayNotice
         noticeData={noticeData ? noticeData : false}
         locationSlug={locationSlug ? locationSlug : false}
-        giftBooking={giftBooking? giftBooking:false}
+        giftBooking={giftBooking ? giftBooking : false}
       />
+      </>}
+        {currentNotice=='uer' && <>
+       <UnlimitedBundleNotice
+        noticeData={noticeData ? noticeData : false}
+        locationSlug={locationSlug ? locationSlug : false}
+        giftBooking={giftBooking ? giftBooking : false}
+      />
+      </>}
+     
     </div>
   );
 }
