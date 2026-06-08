78fa27274d220cdd2d04d3a15d6cb37ec0ddbd98
shihab khan <shihabx7@gmail.com>
2026-06-08 18:21:02 +0600

    final test

--- comps/v2/eventPageV2/elements/LoveCarousel.jsx
+++ comps/v2/eventPageV2/elements/LoveCarousel.jsx
@@ -72,9 +72,37 @@ export default function LoveCarousel({ customerLoveCarouselData }) {
           return (
             <div key={index} className="rev-item">
               {index % 2 == 0 ? (
-                <LoveCarouselCardBrown item={item} />
+                <div className="love-carousel-card bg-[#EBDBB2] h-full flex flex-col space-y-8 rounded-[16px] p-3 xl:p-5 overflow-hidden -rotate-2">
+                  <div className="rev-txt">
+                    <img
+                      className="max-w-[120px]"
+                      src={"/assets/event-page-v2/five-star-rating-icon.svg"}
+                    ></img>
+                    <p className="lg:text-sm xl:text-base text-[#374151] mt-5">
+                      {item.review}
+                    </p>
+                  </div>
+                  <div className="rev-author">
+                    <p className="text-sm text-[#374151] ">{item.authorName}</p>
+                    <p className="text-sm text-[#494949] mt-1">{item.city}</p>
+                  </div>
+                </div>
               ) : (
-                <LoveCarouselCardDark item={item} />
+                <div className="love-carousel-card-dk bg-[#423109] h-full flex flex-col space-y-8 rounded-[16px] p-3 xl:p-5 overflow-hidden rotate-2">
+                  <div className="rev-txt">
+                    <img
+                      className="max-w-[120px]"
+                      src={"/assets/event-page-v2/five-star-rating-icon.svg"}
+                    ></img>
+                    <p className="lg:text-sm xl:text-base text-[#ebebeb] mt-5">
+                      {item.review}
+                    </p>
+                  </div>
+                  <div className="rev-author">
+                    <p className="text-sm text-[#CCCCCC] ">{item.authorName}</p>
+                    <p className="text-sm text-[#9C9C9C] mt-1">{item.city}</p>
+                  </div>
+                </div>
               )}
             </div>
           );
