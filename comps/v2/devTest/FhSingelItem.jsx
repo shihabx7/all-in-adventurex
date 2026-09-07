8d791d39e53a4cbdb535fc7e752ce4b26e42367b
shihab khan <shihabx7@gmail.com>
2026-09-07 16:29:48 +0600

    up

--- comps/v2/devTest/FhSingelItem.jsx
+++ comps/v2/devTest/FhSingelItem.jsx
@@ -4,10 +4,11 @@ import Script from "next/script";
 export default function FhSingleItem() {
   //https://fareharbor.com/embeds/book/allinadventures-raleigh/items/624177/availability/1740215307/book/?flow=no
   //https://fareharbor.com/embeds/book/allinadventures-raleigh/items/53870/calendar/2026/09/?flow=16807&full-items=yes
+  //https://fareharbor.com/embeds/book/kingstontraining/items/56091/?full-items=yes&flow=17842
 
-  const embedCode1 = `<script src="https://fareharbor.com/embeds/script/item/allinadventures-raleigh/item/53870/?flow=16807&full-items=yes"></script>`;
+  const embedCode1 = `<script src="https://fareharbor.com/embeds/script/book/allinadventures-raleigh/items/53870/?flow=16807&full-items=yes"></script>`;
   const embedCode2 = `<script src="https://fareharbor.com/embeds/script/book/allinadventures-raleigh/item/624177/?flow=no&full-items=no"></script>`;
-    const embedCode = `
+  const embedCode = `
     <!-- FareHarbor calendar of item #53870 -->
     <script src="https://fareharbor.com/embeds/script/items/allinadventures-raleigh/items/53870/?fallback=simple&full-items=no&flow=16807"></script>
   `;
@@ -21,10 +22,18 @@ export default function FhSingleItem() {
           <p className="text-gray-300 text-xl font-medium mt-4">
             Embed Grid for Releigh, NC Location with search option
           </p>
+          <div className="fareharbor-booking-btn my-6 flex justify-center items-center">
+            <a
+              className="inline-block  py-3 px-10 rounded-full text-center text-white font-medium border-2 border-red-600 bg-red-600 hover:border-red-700 hover:bg-red-700 text-[15px] rm:text-base md:text-lg"
+              href="https://fareharbor.com/embeds/book/allinadventures-raleigh/items/53870/?full-items=yes&flow=16807"
+            >
+              Book now (Fareharbor Booking- button)
+            </a>
+          </div>
         </div>
         <div className="fareharbor-grid-wrapper w-full min-h-[600px]">
           <div className="fareharbor-grid-wrapper my-6">
-            <div dangerouslySetInnerHTML={{ __html: embedCode2 }} />
+            <div dangerouslySetInnerHTML={{ __html: embedCode1 }} />
           </div>
         </div>
       </div>
